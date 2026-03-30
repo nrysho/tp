f6ded2a2cd500ff2eaba69c47d23831da86143a2
nrysho <neryssho@gmail.com>
2026-03-31 02:26:37 +0800

    Add remark field

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,42 @@
+package seedu.address.model.person;
+
+public class Remark {
+    private final String value;
+
+    /**
+     * Constructs a {@code Remark}.
+     *
+     * @param remark A valid remark.
+     */
+    public Remark(String remark) {
+        this.value = remark;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Remark)) {
+            return false;
+        }
+
+        Remark otherRemark = (Remark) other;
+        return value.equals(otherRemark.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
